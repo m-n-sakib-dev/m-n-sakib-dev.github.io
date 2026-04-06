b5c9dc2645ae264dd9e63203cd8b6d8cf38c46f9
m-n-sakib-dev <mdsakib.ndc17@gmail.com>
2026-04-06 20:28:10 +0600

    next config changes

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+        output: "export",
+        basePath: "/m-n-sakib-dev.github.io",
+        images: {
+                unoptimized: true,
+        },
+};
+
+export default nextConfig;
