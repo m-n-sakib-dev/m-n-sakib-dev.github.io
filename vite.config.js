9a1bcab55e7680656d7838a4aae32e6db47a3af1
m-n-sakib-dev <mdsakib.ndc17@gmail.com>
2026-04-06 20:35:09 +0600

    vite config fixed

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // base: '/' for custom domain or root deployment
 // base: '/repo-name/' for GitHub Pages project site (e.g. '/m-n-sakib-dev.github.io/')
 export default defineConfig({
-  plugins: [react()],
-  base: '/',
+        plugins: [react()],
+        base: "/m_n_sakib-s_portfolio/",
 });
